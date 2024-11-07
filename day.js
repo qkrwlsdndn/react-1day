7731e55e58e5364460998fcdf99f968e3232aa2f
qkrwlsdndn <fhfhfk12@naver.com>
2024-11-07 17:48:07 +0900

    🐛 그냥써봄
    ㅇ

--- day.js
+++ day.js
@@ -6,3 +6,15 @@ const now = dayjs();
 console.log(now.toString());
 
 console.log(now.format("YYYY-MM-DD"));
+
+// 리액트
+
+//  npm i 패키지이름 => 패키지설치
+
+//  npm unistall 패키지이름 -> 패키지 삭제
+
+// npm run은 Node.js 프로젝트에서 스크립트를 실행하기 위한 명령어입니다. 주로 package.json 파일에 정의된 명령어를 쉽게 실행하기 위해 사용됩니다.
+
+// npm ls 패키지 목록 확인
+
+// npx 패키지 이름 -> npx 사용 npx는 패키지를 1회성 으로 사용하려고 사용
